a2247d8b169ce7835febdcf7aad5fedffe9c7d97
netinw <fran.g.p.neto@gmail.com>
2019-03-20 20:16:12 -0300

    keywords

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -51,15 +51,15 @@ function getCookie(cookieName, stringCsookie) {
 export default {
   head() {
     return {
-      title: "Dashboard Mercado Livre - Login",
+      title: "Calei | Dashboard Mercado Livre",
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { name: "keywords", content: "dashboard mercado livre, mercado livre, gestão de vendas mercado livre, gestão de vendas" },
+        { name: "keywords", content: "dashboard mercado livre, mercado livre, gestão de vendas mercado livre, gestão de vendas, vender mais, mercadolivre, software, ferramentas, acompanhar concorrentes, responder perguntas, subir de posição, publicar, publicar massivamente, indicadores, metricas, tempo real, notificações, operadores" },
         {
           hid: "description",
           name: "description",
-          content: "aplicação para gestão de vendas realizadas no mercado livre."
+          content: "Acompanhe seus concorrentes, tenha acesso a indicadores das vendas realizadas no Mercado Livre. Aplicação para gestão de vendas realizadas no Mercado Livre."
         }
       ]
     };
