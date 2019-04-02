9f2ad9c87c40c934176baf8fa5d4b6c9c56249af
netinw <fran.g.p.neto@gmail.com>
2019-04-02 20:08:06 -0300

    ajuste title

--- layouts/clean.vue
+++ layouts/clean.vue
@@ -1,3 +1,27 @@
 <template>
   <nuxt />
 </template>
+<script>
+export default {
+  head() {
+    return {
+      title: "Dashboard Mercado Livre",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "keywords",
+          content:
+            "dashboard mercado livre, mercado livre, gestão de vendas mercado livre, gestão de vendas, vender mais, mercadolivre, software, ferramentas, acompanhar concorrentes, responder perguntas, subir de posição, publicar, publicar massivamente, indicadores, metricas, tempo real, notificações, operadores"
+        },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Acompanhe seus concorrentes, tenha acesso a indicadores das vendas realizadas no Mercado Livre. Aplicação para gestão de vendas realizadas no Mercado Livre."
+        }
+      ]
+    };
+  }
+}
+</script>
